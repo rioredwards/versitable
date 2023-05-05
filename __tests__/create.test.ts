65077fe63451819194f759b130a3fd5f2e134500
Rio Edwards <rioredwards@gmail.com>
2023-05-04 23:51:52 -0700

    Seperate tests into 2 files related to the function they are testing

---
+++ __tests__/create.test.ts
@@ -0,0 +1,64 @@
+import { versitable } from "../src";
+import {
+  invalidTableData,
+  invalidTableOptions,
+} from "./__mocks__/invalidTableData";
+import { validTableData, validTableOptions } from "./__mocks__/validTableData";
+
+describe("versitable.create", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it("should create a string[][] if passed in a string[][]", () => {
+    const table = versitable.create(validTableData);
+    expect(table).toBeInstanceOf(Array);
+    expect(table[0]).toBeInstanceOf(Array);
+    expect(typeof table[0][0]).toBe("string");
+  });
+
+  it("should error if not passed a table with type: string[][]", () => {
+    for (const element of invalidTableData) {
+      expect(() => versitable.create(element)).toThrowError();
+    }
+  });
+
+  it("should create a table if passed in valid options", () => {
+    const table = versitable.create(validTableData, validTableOptions);
+    expect(table).toBeInstanceOf(Array);
+    expect(table[0]).toBeInstanceOf(Array);
+    expect(typeof table[0][0]).toBe("string");
+  });
+
+  it("should error if passed in invalid options (with strict mode)", () => {
+    for (const option of invalidTableOptions) {
+      expect(() =>
+        versitable.create(validTableData, {
+          ...validTableOptions,
+          ...option,
+        } as any)
+      ).toThrowError();
+    }
+  });
+
+  it("should limit the rows created based on the maxRows option", () => {
+    const table = versitable.create(validTableData, {
+      maxRows: 10,
+      maxRowHeight: 1,
+    });
+    expect(table.length).toBeLessThanOrEqual(10);
+  });
+
+  it("should limit the columns created based on the maxColumns option", () => {
+    const table = versitable.create(validTableData, { maxColumns: 2 });
+    expect(table[0].length).toBeLessThanOrEqual(2);
+  });
+
+  it("should truncate the cells based on the maxColWidths option", () => {
+    const table = versitable.create(validTableData, {
+      maxColWidths: 7,
+      cellPadding: 0,
+    });
+    expect(table[0][0].length).toBeLessThanOrEqual(7);
+  });
+});
--- __tests__/options.test.ts
+++ __tests__/options.test.ts
@@ -1,19 +1,14 @@
-import { versitable } from "../src";
 import * as tableValidations from "../src/tableValidations";
 import {
   invalidCellPaddings,
   invalidMaxColWidths,
   invalidMaxColumns,
   invalidMaxRows,
-  invalidTableData,
-  invalidTableOptions,
 } from "./__mocks__/invalidTableData";
 import {
   validCellPaddings,
   validMaxColumns,
   validMaxRows,
-  validTableData,
-  validTableOptions,
 } from "./__mocks__/validTableData";
 
 describe("checkTableOptionsAreValid", () => {
@@ -96,68 +91,9 @@ describe("checkTableOptionsAreValid", () => {
   });
   it("should throw an error if maxColWidths option is invalid", () => {
     invalidMaxColWidths.forEach((maxColWidths: any) => {
-      console.log("maxColWidths: ", maxColWidths);
       expect(() =>
         tableValidations.checkTableOptionsAreValid({ maxColWidths })
       ).toThrowError();
     });
-  });
-});
-
-describe("versitable.create", () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
-
-  it("should create a string[][] if passed in a string[][]", () => {
-    const table = versitable.create(validTableData);
-    expect(table).toBeInstanceOf(Array);
-    expect(table[0]).toBeInstanceOf(Array);
-    expect(typeof table[0][0]).toBe("string");
-  });
-
-  it("should error if not passed a table with type: string[][]", () => {
-    for (const element of invalidTableData) {
-      expect(() => versitable.create(element)).toThrowError();
-    }
-  });
-
-  it("should create a table if passed in valid options", () => {
-    const table = versitable.create(validTableData, validTableOptions);
-    expect(table).toBeInstanceOf(Array);
-    expect(table[0]).toBeInstanceOf(Array);
-    expect(typeof table[0][0]).toBe("string");
-  });
-
-  it("should error if passed in invalid options (with strict mode)", () => {
-    for (const option of invalidTableOptions) {
-      expect(() =>
-        versitable.create(validTableData, {
-          ...validTableOptions,
-          ...option,
-        } as any)
-      ).toThrowError();
-    }
-  });
-
-  it("should limit the rows created based on the maxRows option", () => {
-    const table = versitable.create(validTableData, {
-      maxRows: 10,
-      maxRowHeight: 1,
-    });
-    expect(table.length).toBeLessThanOrEqual(10);
-  });
-
-  it("should limit the columns created based on the maxColumns option", () => {
-    const table = versitable.create(validTableData, { maxColumns: 2 });
-    expect(table[0].length).toBeLessThanOrEqual(2);
-  });
-
-  it("should truncate the cells based on the maxColWidths option", () => {
-    const table = versitable.create(validTableData, {
-      maxColWidths: 7,
-      cellPadding: 0,
-    });
-    expect(table[0][0].length).toBeLessThanOrEqual(7);
   });
 });
